bfe347b60c7154f954f5ed9249893e54595ac625
Guillaume Chauvat <guillaume@chauvat.eu>
2021-02-22 14:08:53 +0100

    Default to Poiseuille at stability limit

--- main.m
+++ main.m
@@ -18,6 +18,7 @@
 %
 %
 
+close all;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%
@@ -27,7 +28,7 @@
 
 %%%  set the flow type:
 %%%  iflow=1 is Poiseuille, iflow=2 is Couette
-iflow=2;
+iflow=1;
 
 %%% time parameters for transient growth
 iflag=1; % find the time for maximal growth
@@ -45,9 +46,9 @@ ylen=20;
 m=0;
 Nprof=99; Lprof=39;
 %%% general variables
-R=25;
-nosmod=100;
-alpha=1;
+R=5772;
+nosmod=120;
+alpha=1.02;
 beta=0;
 k2=alpha^2+beta^2;
 
