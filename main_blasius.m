996f72a8b33e245a6591ad33271486976cce9f5e
Guillaume Chauvat <guillaume@chauvat.eu>
2021-02-23 08:54:49 +0100

    separate main for different cases

--- main_blasius.m
+++ main_blasius.m
@@ -28,28 +28,28 @@ close all;
 
 %%%  set the flow type:
 %%%  iflow=1 is Poiseuille, iflow=2 is Couette
-iflow=1;
+iflow=3;
 
 %%% time parameters for transient growth
 iflag=1; % find the time for maximal growth
-T = 100;
+T = 300;
 
 %iflag=2; T=40;
 %iflag=2 to have the initial and final optimal
 
 %%% parameters for frequency response
-omega=[-.1 1.2];
+omega=[-.05 0.35];
 
 
 %%% blasius specific parameters
-ylen=20;
+ylen=30;
 m=0;
-Nprof=99; Lprof=39;
+Nprof=160; Lprof=70;
 %%% general variables
-R=5772;
-nosmod=120;
-alpha=1.02;
-beta=0;
+R=550;
+nosmod=150;
+alpha=0.24;
+beta=0.;
 k2=alpha^2+beta^2;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
